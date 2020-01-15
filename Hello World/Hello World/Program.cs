3a9d3e512f517d8addefa94de89989ca84e3835b
cat4181 <cat4181@IGMOH1370-10>
2020-01-15 10:38:33 -0500

    hello world

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    // Cie Thompson
+    // Class: Program
+    // The main class for this program containing my main method
+    // Restrictions: none
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print hello world to the console
+            Console.WriteLine("Hello World!");
+
+            /* My name is Cie Thompson
+             * It seems important to add my name here
+             * so I did.
+             */
+            
+            Console.WriteLine("Cie Thompson");
+
+            /* For this, do I really need the ReadLine here?
+             * When running without debugging it already has the
+             * "Press any key to continue..." when it reaches the end
+             * so it seems kinda redundant to me but idk.
+             */
+
+            Console.ReadLine();
+        }
+    }
+}
