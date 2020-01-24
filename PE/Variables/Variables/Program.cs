1fce0f7564080194273d2fae27db331d950620c6
Cie Thompson <55000443+wink128@users.noreply.github.com>
2020-01-24 04:31:13 -0500

    done

--- PE/Variables/Variables/Program.cs
+++ PE/Variables/Variables/Program.cs
@@ -23,11 +23,46 @@ namespace Variables
             Console.WriteLine("{0} is starting the game with {1} resources.",
                 playerName, startingResources);
             Console.WriteLine(">> Current Resources: {0}", currentResources);
+            Console.WriteLine();
 
             // new ship + weapon upgrade calculation
             Console.WriteLine("{0} purchased a ship for {1} resources",
-                shipPrice, weaponUpgrade);
+                playerName, shipPrice);
+            currentResources-=shipPrice;
+            Console.WriteLine("{0} purchased a weapons upgrade for {1} resources",
+                playerName, weaponUpgrade);
+            currentResources-=weaponUpgrade;
+
             Console.WriteLine(">> Current Resources: {0}", currentResources);
+            Console.WriteLine();
+
+            // missile purchase
+            Console.WriteLine("{0} can purchase 3 missiles with their remaining"+
+                " resources, so they did.", playerName);
+            currentResources-=(MissileCost*3);
+            Console.WriteLine(">> Current Resources: {0}", currentResources);
+            Console.WriteLine();
+
+            // alien kills
+            Console.WriteLine("{0} killed 7 aliens, earning 35 resources.", 
+                playerName);
+            currentResources+=(AmountEarnedPerKill*7);
+            Console.WriteLine(">> Current Resources: {0}", currentResources);
+            Console.WriteLine();
+
+            // selling and stealing gear
+            Console.WriteLine("{0} stole and sold gear for 112 resources.", 
+                playerName);
+            currentResources+=112;
+            Console.WriteLine(">> Current Resources: {0}", currentResources);
+            Console.WriteLine();
+
+            // hangar space leasing
+            Console.WriteLine("{0} leased a hangar space for a month for "+
+                "20 resources.", playerName);
+            currentResources-=20;
+            Console.WriteLine(">> Current Resources: {0}", currentResources);
+            Console.WriteLine();
 
             // for debug
             Console.ReadLine();
