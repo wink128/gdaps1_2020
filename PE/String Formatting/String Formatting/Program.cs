d0e7204930eb3f15ac703cdd742ca5cc50d4d8b4
cat4181 <cat4181@IGMOH1370-01>
2020-01-29 10:46:45 -0500

    almost done

--- PE/String Formatting/String Formatting/Program.cs
+++ PE/String Formatting/String Formatting/Program.cs
@@ -14,6 +14,7 @@ namespace String_Formatting
 
         static void Main(string[] args)
         {
+            /*
             // Name Analysis
             Console.Write("I'd like to analyze your first name. What is it? ");
             Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -29,9 +30,27 @@ namespace String_Formatting
             Console.WriteLine("The last letter of your name is {0}",
                 name.Substring((name.Length)-1,name.Length));
             Console.WriteLine();
+            */
+            // ToString()
+            Console.Write("Enter a number with a decimal value: ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            double decimalValue = double.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nHere are your results using the ToString() method:");
+            Console.WriteLine("{0}\n{1}\n{2}\n", 
+                decimalValue.ToString("F2"),
+                decimalValue.ToString("P4"),
+                decimalValue.ToString("F0"));
 
+            // String.Format()
+            Console.WriteLine("Here are your results using the String.Format() method:");
+            string newString = String.Format("{0:F2}\n{1:P4}\n{2:F0}\n",
+                decimalValue, decimalValue, decimalValue);
+            Console.WriteLine(newString);
 
-
+            // Console.WriteLine()
+            Console.WriteLine("Here are your results using the Console.WriteLine() method:");
+            Console.WriteLine("{0:F2}\n{1:P4}\n{2:F0}\n", decimalValue, decimalValue, decimalValue);
         }
     }
 }
