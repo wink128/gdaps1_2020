f662494d2a2836c4765421eba58d373543cf247b
Cie Thompson <55000443+wink128@users.noreply.github.com>
2020-02-03 17:20:52 -0500

    not done just lazy

--- PE/If Statements/If Statements/Program.cs
+++ PE/If Statements/If Statements/Program.cs
@@ -21,38 +21,55 @@ namespace If_Statements
                 Console.WriteLine("Your choices are: a PLAYER, a FRIENDLY mob," +
                     " a HOSTILE mob, or NOTHING\n");
                 Console.Write("Creeper, what do you see? ");
+                Console.ForegroundColor = ConsoleColor.Green;
                 sight = Console.ReadLine().Trim().ToUpper();
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine();
 
                 if (sight == "PLAYER")
                 {
-                    Console.Write("How far away are they? ");
+                    Console.Write("How many blocks away are they? ");
+                    Console.ForegroundColor = ConsoleColor.Green;
                     int distance = int.Parse(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine();
 
                     if (distance <= 3)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Explode.");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine();
                     }
                     else
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Follow them.");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine();
                     }
                 }
                 else if (sight == "FRIENDLY")
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Try to turn them to the dark side.");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine();
                 }
                 else if (sight == "HOSTILE")
                 {
                     Console.WriteLine("Go make friends!");
+                    Console.WriteLine();
                 }
                 else if (sight == "HEROBRINE")
                 {
                     Console.WriteLine("*GASP* HE LIVES");
+                    Console.WriteLine();
                 }
                 else
                 {
-                    Console.WriteLine("Despawn.");
-                    break;
+                    Console.WriteLine("YOu'Re hAcKinG");
+                    Console.WriteLine();
                 }
             } while (sight != "NOTHING");
         }
