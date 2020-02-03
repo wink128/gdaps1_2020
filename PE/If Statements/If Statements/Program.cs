a549ae3a6ae568de4d0add385898e9bb3c7830cc
cat4181 <cat4181@IGMOH1370-01>
2020-02-03 10:06:49 -0500

    started

---
+++ PE/If Statements/If Statements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements
+{
+    class Program
+    {
+        // Cie Thompson
+        // 2/3/19
+        // If Statements PE
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Character: Creeper (aww man)");
+            Console.WriteLine("Your choices are: a PLAYER, a FRIENDLY mob," +
+                " a HOSTILE mob, or NOTHING\n");
+            Console.Write("Creeper, what do you see? ");
+            
+
+
+        }
+    }
+}
