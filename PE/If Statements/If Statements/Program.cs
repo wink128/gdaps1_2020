907221f0ea62be87f9cf82ae94b69a2f1cd3b779
Cie Thompson <55000443+wink128@users.noreply.github.com>
2020-02-03 18:50:43 -0500

    fixed

--- PE/If Statements/If Statements/Program.cs
+++ PE/If Statements/If Statements/Program.cs
@@ -17,13 +17,37 @@ namespace If_Statements
             string sight;
             do
             {
-                Console.WriteLine("Character: Creeper (aww man)");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("Character: ");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("Creeper ");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                // sorry I had to do it for the meme
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("(");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("a");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("w");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("w ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("m");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("a");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("n");
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine(")");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
                 Console.WriteLine("Your choices are: a PLAYER, a FRIENDLY mob," +
-                    " a HOSTILE mob, or NOTHING\n");
+                    " a HOSTILE mob, HEROBRINE, or NOTHING\n");
                 Console.Write("Creeper, what do you see? ");
                 Console.ForegroundColor = ConsoleColor.Green;
                 sight = Console.ReadLine().Trim().ToUpper();
-                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine();
 
                 if (sight == "PLAYER")
@@ -31,21 +55,32 @@ namespace If_Statements
                     Console.Write("How many blocks away are they? ");
                     Console.ForegroundColor = ConsoleColor.Green;
                     int distance = int.Parse(Console.ReadLine());
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.WriteLine();
 
                     if (distance <= 3)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Explode.");
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine();
+                    }
+                    else if (distance > 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Follow them.");
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
                         Console.WriteLine();
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Follow them.");
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("???");
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine();
+                        Console.Write("How many blocks away are they? ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        distance = int.Parse(Console.ReadLine());
                         Console.WriteLine();
                     }
                 }
@@ -53,18 +88,29 @@ namespace If_Statements
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Try to turn them to the dark side.");
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.WriteLine();
                 }
                 else if (sight == "HOSTILE")
                 {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Go make friends!");
                     Console.WriteLine();
                 }
                 else if (sight == "HEROBRINE")
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("*GASP* HE LIVES");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.WriteLine();
+                }
+                else if (sight == "NOTHING")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Despawn.");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine();
+                    break;
                 }
                 else
                 {
