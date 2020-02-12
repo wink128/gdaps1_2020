b90779a73aae28f36f0090f3a1a13c93a6139226
cat4181 <cat4181@IGMOH1370-01>
2020-02-12 09:23:00 -0500

    nyeh

---
+++ Demos/for loops/for loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a positive whole number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            while (!(input > 0))
+            {
+                Console.Write("Invalid. Enter a positive whole number: ");
+                input = int.Parse(Console.ReadLine());
+            }
+
+            if (input > 0)
+            {
+                int counter = 0;
+                while (counter < input + 1)
+                {
+                    Console.Write(counter + " ");
+                    counter++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
