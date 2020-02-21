4c36587b4d8b9b339a98101ad2785095558c5d8e
Cie Thompson <55000443+wink128@users.noreply.github.com>
2020-02-20 23:16:28 -0500

    done!

--- Homework/HW2_Superman/HW2_Superman/Program.cs
+++ Homework/HW2_Superman/HW2_Superman/Program.cs
@@ -14,35 +14,64 @@ namespace HW2_Superman
 
         static void Main(string[] args)
         {
-            // introduction
-            Console.WriteLine("Welcome to the Superman Jump simulator!\n");
-            Console.WriteLine("Your target building height is 660 feet.\n");
-            Console.WriteLine("Please enter the gravitational constant for the " +
-                "planet on which Superman is\ncurrently attempting this jump. " +
-                "The units are in ft/s^2\n");
-            Console.WriteLine();
-
-            double constant;
-            bool success;
-
-            // guess validation
-            Console.Write("Gravitational Constant: ");
-            success = double.TryParse(Console.ReadLine().Trim(), out constant);
-            Console.WriteLine();
-
-            while (!success || constant < 0)
+            string playAgain = null;
+            while (playAgain != "N")
             {
-                Console.WriteLine("Invalid value. Enter a positive number.\n");
+
+                // introduction
+                Console.WriteLine("Welcome to the Superman Jump simulator!\n");
+                Console.WriteLine("Your target building height is 660 feet.\n");
+                Console.WriteLine("Please enter the gravitational constant for the " +
+                    "planet on which Superman is\ncurrently attempting this jump. " +
+                    "The units are in ft/s^2\n");
+
+                double gravConst = 0;
+                bool success;
+
+                // guess validation
                 Console.Write("Gravitational Constant: ");
-                success = double.TryParse(Console.ReadLine().Trim(), out constant);
+                success = double.TryParse(Console.ReadLine().Trim(), out gravConst);
                 Console.WriteLine();
+
+                while (!success || gravConst < 0)
+                {
+                    Console.WriteLine("Invalid value. Enter a positive number.\n");
+                    Console.Write("Gravitational Constant: ");
+                    success = double.TryParse(Console.ReadLine().Trim(), out gravConst);
+                    Console.WriteLine();
+                }
+
+                // Calculations + display
+                double initialVelocity = Math.Sqrt(2 * gravConst * 660);
+                Console.WriteLine("Superman must jump with an initial velocity of at least {0} ft/s.",
+                    initialVelocity);
+                Console.WriteLine();
+
+                // another value?
+                Console.Write("Would you like to play again? (Y/N): ");
+                playAgain = Console.ReadLine().Trim().ToUpper();
+                
+                while (playAgain != "Y" && playAgain != "N")
+                {
+                    Console.WriteLine("Invalid value.");
+                    Console.Write("Would you like to play again? (Y/N): ");
+                    playAgain = Console.ReadLine().Trim().ToUpper();
+                }
+
+                if (playAgain == "N")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you for playing!");
+                    Console.WriteLine();
+                    break;
+                }
+                else if (playAgain == "Y")
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+
             }
-
-            // Calculations
-
-            // another value?
-
-
         }
     }
 }
