c2118dcc7e8a1f72bb293e97aaf48de930e92651
cat4181 <cat4181@IGMOH1370-01>
2020-02-24 10:52:33 -0500

    not done yet

--- PE/2D Arrays/2D Arrays/Program.cs
+++ PE/2D Arrays/2D Arrays/Program.cs
@@ -10,6 +10,46 @@ namespace _2D_Arrays
     {
         static void Main(string[] args)
         {
+            // Array declaration + initialization
+            char[,] grid = new char[10, 15];
+
+            // Hard coded words
+            grid[1, 7] = (char)109;   // m
+            grid[1, 8] = (char)105;   // i
+            grid[1, 9] = (char)109;   // m
+            grid[1, 10] = (char)105;  // i
+            grid[1, 11] = (char)107;  // k
+            grid[1, 12] = (char)121;  // y
+            grid[1, 13] = (char)117;  // u
+
+            grid[4, 4] = (char)115;   // s
+            grid[4, 5] = (char)108;   // l
+            grid[4, 6] = (char)117;   // u
+            grid[4, 7] = (char)103;   // g
+            grid[4, 8] = (char)109;   // m
+            grid[4, 9] = (char)97;    // a
+
+            grid[7, 1] = (char)112;   // p
+            grid[7, 2] = (char)111;   // o
+            grid[7, 3] = (char)107;   // k
+            grid[7, 4] = (char)101;   // e
+            grid[7, 5] = (char)109;   // m
+            grid[7, 6] = (char)111;   // o
+            grid[7, 7] = (char)110;   // n
+
+            // rng letters
+            // the ASCII codes are between 97 and 122
+
+            // Print Grid
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
