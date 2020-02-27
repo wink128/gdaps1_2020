b72faea5f999bae0195c5c41238cd40cc7b69c5a
cat4181 <cat4181@IGM2570-06>
2020-02-27 16:23:23 -0500

    meh

---
+++ PE/Casting Math n Documentation/Casting Math n Documentation/MathMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting_Math_n_Documentation
+{
+    class MathMethods
+    {
+    }
+}
